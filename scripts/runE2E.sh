9eb3987d54dda89507865f27f6ffd5b1a1feab88
Grigorovsky Anton <agrigorovskiy@loylabs.ru>
2023-11-05 11:42:08 +0300

    run e2e script

---
+++ scripts/runE2E.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+rootDir="$currentDir/../"
+
+(cd "$rootDir" && exec ./gradlew -p tests/e2e test allureReport  --continue --clean)
