89ca33b33f6672d83c71241d51601078972efd8e
Grigorovsky Anton <agrigorovskiy@loylabs.ru>
2023-11-26 17:43:09 +0300

    containerize e2e tests

--- scripts/runE2E.sh
+++ scripts/runE2E.sh
@@ -3,4 +3,5 @@ set -e
 currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 rootDir="$currentDir/../"
 
-(cd "$rootDir" && exec ./gradlew -p tests/e2e test allureReport  --continue --clean)
+(cd "$rootDir" && exec ./gradlew -p tests/e2e test allureReport  --continue --clean \
+ -DstartDocker=false -DenvFile=`pwd`/config/docker/env/e2e.env)
--- tests/e2e/src/test/java/com/stringconcat/people/e2e/cases/CreatePersonCase.java
+++ tests/e2e/src/test/java/com/stringconcat/people/e2e/cases/CreatePersonCase.java
@@ -1,5 +1,7 @@
 package com.stringconcat.people.e2e.cases;
 
+import com.stringconcat.people.common.StandConfiguration;
+import com.stringconcat.people.common.StandContainer;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
 import io.restassured.RestAssured;
@@ -15,15 +17,20 @@ import static com.stringconcat.people.e2e.steps.PersonSteps.getPersonByUrl;
 @Epic("Create a person")
 public class CreatePersonCase {
 
+    private final StandConfiguration settings = new StandConfiguration();
+    private final StandContainer standContainer = new StandContainer(settings);
+
     @Test
     @Story("Create a person")
     public void createPersonTest() {
-        RestAssured.baseURI = "http://localhost:8080";
+        standContainer.start();
+        RestAssured.baseURI = settings.peopleBaseUrl;
 
         getForm("/generate");
         var id = createPerson("/generate?firstName=" + FIRST_NAME +
                 "&secondName=" + SECOND_NAME +
                 "&birthDate=" + BIRTH_DATE + "&gender=male");
         getPersonByUrl("/id/" + id);
+        standContainer.stop();
     }
 }
